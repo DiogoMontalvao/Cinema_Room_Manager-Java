6bf8034bac7571c5f7326c188904bea9d22de099
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2023-12-23 19:11:24 -0300

    Work on project. Stage 1/5: Arrangement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int numero = 10;
-        char letra = 'A';
-        String palavra = "Olar";
-
-        System.out.println(numero + palavra + letra);
+        System.out.println("Cinema:");
+        System.out.println("  1 2 3 4 5 6 7 8");
+        System.out.println("1 S S S S S S S S");
+        System.out.println("2 S S S S S S S S");
+        System.out.println("3 S S S S S S S S");
+        System.out.println("4 S S S S S S S S");
+        System.out.println("5 S S S S S S S S");
+        System.out.println("6 S S S S S S S S");
+        System.out.println("7 S S S S S S S S");
     }
 }
