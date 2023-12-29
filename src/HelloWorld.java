114b6351b01ce8749db327c5408421d1be2901e4
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2023-12-28 22:02:57 -0300

    Att

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,14 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        System.out.println("1 S S S S S S S S");
-        System.out.println("2 S S S S S S S S");
-        System.out.println("3 S S S S S S S S");
-        System.out.println("4 S S S S S S S S");
-        System.out.println("5 S S S S S S S S");
-        System.out.println("6 S S S S S S S S");
-        System.out.println("7 S S S S S S S S");
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt(), b = scanner.nextInt(), c = scanner.nextInt(), d = scanner.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
