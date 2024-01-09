ce8a5cc45a529dea0351772e8f30cb041a1f73c3
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2024-01-09 19:32:56 -0300

    createCube

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] array = new int[][] {
-                { 1 },
-                { 0 },
-                { 3 }
-        };
+        System.out.println(Arrays.toString(createCube()));
+    }
 
-        System.out.print(array[0][0] + " " + array[0][array[0].length - 1]);
-        System.out.println();
-        System.out.print(array[array.length - 1][0] + " " + array[array.length - 1][array[array.length - 1].length - 1]);
+    public static int[][][] createCube(){
+
+        return new int[][][] {
+                { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } },
+                { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } },
+                { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } }
+        };
     }
 }
