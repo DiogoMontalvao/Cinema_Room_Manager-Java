57cbff758e7b67fa39e3a5780e063af2e219b9a1
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2024-01-09 17:04:57 -0300

    Find the corners

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int[][] array = new int[][] {
+                { 1 },
+                { 0 },
+                { 3 }
+        };
 
-        int n = scanner.nextInt();
-        System.out.println(((n + 1) * n + 2) * n + 3);
+        System.out.print(array[0][0] + " " + array[0][array[0].length - 1]);
+        System.out.println();
+        System.out.print(array[array.length - 1][0] + " " + array[array.length - 1][array[array.length - 1].length - 1]);
     }
 }
