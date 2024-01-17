f70bfc3acd32e4a92851c90ec29f41901d29e6ea
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2024-01-16 21:37:31 -0600

    Check if an array contains two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(createCube()));
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    public static int[][][] createCube(){
+        int tamanhoArray = scanner.nextInt();
 
-        return new int[][][] {
-                { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } },
-                { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } },
-                { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } }
-        };
+        int[] array = new int[tamanhoArray];
+        for (int i = 0; i < tamanhoArray; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        boolean ocorreProximo = false;
+
+        int n = scanner.nextInt(), m = scanner.nextInt();
+        int atual, proximo, proximoIndice = 1;
+
+        for (int i = 0; i < tamanhoArray; i++) {
+            atual = array[i];
+            proximo = array[proximoIndice];
+
+            if (proximoIndice < tamanhoArray - 1) {
+                proximoIndice++;
+            }
+
+            if (atual == n && proximo == m || proximo == n && atual == m) {
+                ocorreProximo = true;
+            }
+        }
+
+        System.out.println(ocorreProximo);
     }
 }
