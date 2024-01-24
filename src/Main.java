f38a242b3ace275eb67e9c3365a25272c50f6824
Diogo Montalvão <109600744+DiogoMontalvao@users.noreply.github.com>
2024-01-24 15:58:08 -0300

    The army of units

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int tamanhoArray = scanner.nextInt();
+        int armyNumber = scanner.nextInt();
 
-        int[] array = new int[tamanhoArray];
-        for (int i = 0; i < tamanhoArray; i++) {
-            array[i] = scanner.nextInt();
+        if (armyNumber < 1) {
+            System.out.println("no army");
+        } else if (armyNumber >= 1 && armyNumber <= 19) {
+            System.out.println("pack");
+        } else if (armyNumber >= 20 && armyNumber <= 249) {
+            System.out.println("throng");
+        } else if (armyNumber >= 250 && armyNumber <= 999) {
+            System.out.println("zounds");
+        } else {
+            System.out.println("legion");
         }
-
-        int valorMaximo = 0;
-        int indiceValorMaximo = 0;
-
-        for (int i = 0; i < array.length; i++){
-            if (array[i] > valorMaximo && indiceValorMaximo != i) {
-                valorMaximo = array[i];
-                indiceValorMaximo = i;
-            }
-        }
-
-        System.out.println(indiceValorMaximo);
-
-        scanner.close();
     }
 }
